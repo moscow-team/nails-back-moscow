11d4aeae6fb97dea93dcc1b7911328ab1415d732
Nicolas Ludueña <nicolascluduena@gmail.com>
2024-11-16 11:28:58 -0300

    domain refactor

--- src/main/java/jsges/nails/domain/articulos/ArticuloVenta.java
+++ src/main/java/jsges/nails/domain/articulos/ArticuloVenta.java
@@ -12,6 +12,8 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ArticuloVenta {
+        final int ESTADO_ACTIVO = 0;
+        final int ESTADO_ELIMINADO = 1;
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,7 +21,9 @@ public class ArticuloVenta {
 
         @Column(columnDefinition = "TEXT")
         String denominacion;
-        int estado;
+
+        @Column(columnDefinition = "INTEGER", nullable = false)
+        private int estado = 0;
 
         @Column(columnDefinition = "TEXT")
         String observacion;
@@ -28,6 +32,13 @@ public class ArticuloVenta {
         private Linea linea;
 
         public void asEliminado() {
-                this.setEstado(1);
+                this.setEstado(ESTADO_ELIMINADO);
+        }
+
+        public void setEstado(int estado) {
+                if (estado != ESTADO_ACTIVO && estado != ESTADO_ELIMINADO) {
+                        throw new IllegalArgumentException("El estado solo puede ser 0 (activo) o 1 (eliminado).");
+                }
+                this.estado = estado;
         }
 }

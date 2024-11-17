47933c4799cb6cd9eb729234c3063058eba5860c
stefbortolin <stefbortolin@gmail.com>
2024-11-17 20:12:15 -0300

    fix entity cliente

--- src/main/java/jsges/nails/domain/organizacion/Cliente.java
+++ src/main/java/jsges/nails/domain/organizacion/Cliente.java
@@ -1,8 +1,10 @@
 package jsges.nails.domain.organizacion;
 
 import jakarta.persistence.*;
+import jsges.nails.domain.ObjetoEliminable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -11,19 +13,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Cliente implements Serializable {
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Cliente extends ObjetoEliminable implements Serializable {
+        
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Integer id;
 
         @Column(columnDefinition = "TEXT")
-
         String razonSocial;
-        int estado;
 
         @Column(columnDefinition = "TEXT")
         String letra;
@@ -33,10 +35,14 @@ public class Cliente implements Serializable {
 
         @Column(columnDefinition = "TEXT")
         String celular;
+
         @Column(columnDefinition = "TEXT")
         String mail;
 
+        @Column(nullable = false)
         Date fechaInicio;
+
+        @Column(nullable = false)
         Date fechaNacimiento;
 
 
